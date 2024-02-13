1c8423707d94a90120963df411f1d127c5986a20
aniru345 <138832503+aniru345@users.noreply.github.com>
2024-02-13 08:53:10 -0800

    Create CPX-CPB-YourNewName.py

---
+++ CPX-CPB-YourNewName.py
@@ -0,0 +1,31 @@
+
+import time
+import board
+import neopixel
+
+#10 neopixels
+num_pixels = 10
+
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels)
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+def wheel(pos):
+    if pos < 85:
+        return (pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return (255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return (0, pos * 3, 255 - pos * 3)
+
+while True:
+    rainbow_cycle(0.1)
